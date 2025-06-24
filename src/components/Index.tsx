51dd3430eccbb9ae4cfb7237240949738d74f5fc
juanvarrone <pichichus14@gmail.com>
2025-06-24 16:03:00 +0000

    ok margen

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -24,7 +24,7 @@ const Index = () => {
   if (isAdmin) {
     return (
       <>
-        <section className="py-12 mb-8">
+        <section className="py-1 mb-1">
           <div className="text-center max-w-3xl mx-auto">
          {/*  <div className="flex justify-center mb-6">
               <Logo size="large" />
